45e166b0c105c8b3278b39e868292db13518ff9a
RF <49386140+OSSAVIVI@users.noreply.github.com>
2020-08-25 09:29:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Google import  Google
 
 
 def main():
-    print("Welcome to my Python Bot (it'll delete all yer' YouTube comments. Savvy?)!\n")
+    print("Welcome to my Python Bot!\n")
     username = "DON'T ENTER USERNAME HERE - DO IT IN DeleteYTCommentsBot.py starting at line #96 (will fix later!) "
     password = "PASSWORD" # enter password here
     Google(username, password)
